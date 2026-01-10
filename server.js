6ff9dd8a50633b79ab00864b0ee5d4cc7927df26
ealbanca <ealbanca@byui.edu>
2026-01-09 19:28:19 -0700

    Added the routes folder

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // express web server
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1');
-
-app.get('/', lesson1Controller.jacobRoute);
 
 const port =3000;
+
+app.use('/', require('./routes/index'));
 
 app.listen(process.env.PORT || port);
 console.log('Server is running ' +'on port ' + (process.env.PORT || port));
