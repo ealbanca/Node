1234f1ea18ea5d3ba1b8691cca70a1a3666e3634
ealbanca <ealbanca@byui.edu>
2026-01-09 19:08:50 -0700

    Complete assignment 1

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // express web server
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port =3000;
+
+app.listen(process.env.PORT || port);
+console.log('Server is running ' +'on port ' + (process.env.PORT || port));
