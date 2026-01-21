17638a85dd6ee93258fde1b179b3adfe3684bc92
ealbanca <ealbanca@byui.edu>
2026-01-20 20:58:24 -0700

    Added routes on the contacts.js file

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,9 +4,16 @@ const router = express.Router();
 // Import contacts controller
 const contactsController = require('../controllers/contacts');
 
-// Define routes for contacts
+// Routes for contacts
+//Get all contacts
 router.get('/', contactsController.getAllContacts);
-
+//Get single contact by ID
 router.get('/:id', contactsController.getSingleContact);
+//Post a new contact
+router.post('/', contactsController.createContact);
+//Update a contact by ID
+router.put('/:id', contactsController.updateContact);
+//Delete a contact by ID
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
