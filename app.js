b04fcc5e4a3e0f682e9a395fa434af6e896e6528
ealbanca <ealbanca@byui.edu>
2026-01-31 20:55:23 -0700

    Updated app.js to be able to use the react app

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)) // Swagg
 app.use(BodyParser.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 })
 // routes
